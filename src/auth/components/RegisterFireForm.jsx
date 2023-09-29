e99bd18f196509482783d7d24c0621ee33a1ebea
carlOzil <seifer01es@msn.com>
2023-09-29 10:55:28 +0200

    fetch forms

--- src/auth/components/RegisterFireForm.jsx
+++ src/auth/components/RegisterFireForm.jsx
@@ -1,49 +1,53 @@
 import React from 'react';
 import { useForm } from '../../hooks/useForm';
+import { useFetch } from '../../hooks/useFetch';
 
 export const RegisterFireForm = () => {
-    const { data, handleSubmit, handleChange } = useForm()
-    // FALTA ENVIO A FIREBASE CON FECTCH
+
+  const { data, handleSubmit, handleChange } = useForm()
+
+  const register = () => {
+    const url = 'http://localhost:3000/api/v1/auth/register';
+
+    const registro = useFetch(url, 'POST', data)
+    console.log(registro)
+
+  };
+
+  useEffect(() => {
+    register()
+
+  }, [handleSubmit])
+
   return (
     <div>
-        <h1>Registro</h1>
+      <h1>Registro</h1>
 
-    <form onSubmit={handleSubmit} method='POST'>
-      <div>
-        <label for="email">Correo electrónico</label>
-        <input
-          type="text"
-          id="email"
-          name="email"
-          placeholder='Introduce email..'
-          value={data}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label for="password">Contraseña</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder='Introduce contraseña..'
-          value={data}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label for="confirmPassword">Confirmar contraseña</label>
-        <input
-          type="password"
-          id="confirmPassword"
-          name="confirmPassword"
-          placeholder='Confirma contraseña..'
-          value={data}
-          onChange={handleChange}
-        />
-      </div>
-      <input type="submit" value='enviar' />
-    </form>
+      <form onSubmit={handleSubmit} method='POST'>
+        <div>
+          <label for="email">Correo electrónico</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder='Introduce email..'
+            value={data}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label for="password">Contraseña</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder='Introduce contraseña..'
+            value={data}
+            onChange={handleChange}
+          />
+        </div>
+        <input type="submit" value='enviar' />
+      </form>
     </div>
   );
 };
