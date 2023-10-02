be4425e0f997cab7d881cfc3fa2915db7be278ad
carlOzil <seifer01es@msn.com>
2023-10-02 08:32:41 +0200

    last fixes

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -41,23 +41,23 @@ export const Producto = ({
                         <p className='card-subtitle'>{zona_geografica} - {fecha_anuncio}</p>
                         <p>{descripcion}</p>
                     </div>
-                    <a className='btn btn-primary' href={enlace_pago}>Comprar</a>
+                    <a className='btn btn-success' href={enlace_pago}>Comprar</a>
                 </article>
                 <BackButton />
             </section>
             <div>
 
                 {showEditButton && (
-                    <Link to={`/user/editar/${id_anuncio}`}>Editar</Link>
+                    <Link className='btn btn-dark' to={`/user/editar/${id_anuncio}`}>Editar</Link>
                 )}
 
 
                 {showBuyButton && (
-                    <Link to={{ state: { enlace_pago: true } }}>Comprar</Link>
+                    <Link className='btn btn-success'to={{ state: { enlace_pago: true } }}>Comprar</Link>
                 )}
 
                 {showRegisterLink && (
-                    <Link to="/auth/login">Registro</Link>
+                    <Link className='btn btn-dark' to="/auth/login">Registro</Link>
                 )}
             </div>
         </>
